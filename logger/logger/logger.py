f0600dd483ddd9dee21b027d35f904ea42b4bb5d
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-05-06 17:38:01 +0700

    add cotrol sub

--- logger/logger/logger.py
+++ logger/logger/logger.py
@@ -1,12 +1,10 @@
 import rclpy
 from rclpy.node import Node
-import tf2_ros
-from std_msgs.msg import String
+
 from geometry_msgs.msg import Twist
 from tf2_msgs.msg import TFMessage
 
-
-
+from logger.utils import euler_from_quaternion
 
 class Logger(Node):
     """
@@ -20,15 +18,7 @@ class Logger(Node):
       
         self.declare_logger_parameters()
         self.get_logger_parametes()
-
-        self.subscription = self.create_subscription(
-            TFMessage,
-            self.tf_topic,
-            self.listener_callback,
-            1
-        )
-        
-        self.subscription  # prevent unused variable warning
+        self.declare_logger_subs()
 
     def declare_logger_parameters(self):
         """
@@ -36,6 +26,7 @@ class Logger(Node):
         """
         self.declare_parameter('output_path', "")
         self.declare_parameter('output_folder', 'output_data')
+        self.declare_parameter('control_topic', '/cmd_vel')
         self.declare_parameter('tf_topic', '/tf')
         self.declare_parameter('parent_frame', 'odom')
         self.declare_parameter('robot_frame', 'base_link')
@@ -47,13 +38,35 @@ class Logger(Node):
         """
         self.output_path = self.get_parameter('output_path').get_parameter_value().string_value
         self.output_folder = self.get_parameter('output_folder').get_parameter_value().string_value
+        self.control_topic = self.get_parameter('control_topic').get_parameter_value().string_value
         self.tf_topic = self.get_parameter('tf_topic').get_parameter_value().string_value
         self.parent_frame = self.get_parameter('parent_frame').get_parameter_value().string_value
         self.robot_frame = self.get_parameter('robot_frame').get_parameter_value().string_value
         self.kinetic_model_frame = self.get_parameter('kinetic_model_frame').get_parameter_value().string_value
 
+    def declare_logger_subs(self):
+        """
 
-    def listener_callback(self, msg):
+        """
+        self.tf_sub = self.create_subscription(
+            TFMessage,
+            self.tf_topic,
+            self.tf_callback,
+            1
+        )
+
+        self.control_sub = self.create_subscription(
+            Twist,
+            self.control_topic,
+            self.control_callback,
+            1
+        )
+
+        # prevent unused variable warning
+        self.control_sub
+        self.tf_sub
+
+    def tf_callback(self, msg):
         """
         
         """
@@ -63,10 +76,22 @@ class Logger(Node):
                 transform.child_frame_id == self.robot_frame
             ):  
                 print("Translation")   
-                print(transform.transform.translation)
-                print("Rotation")
-                print(transform.transform.rotation)
+                print("x = {}".format(transform.transform.translation.x))
+                print("y = {}".format(transform.transform.translation.y))
+                yaw = euler_from_quaternion(
+                    transform.transform.rotation.x,
+                    transform.transform.rotation.y,
+                    transform.transform.rotation.z,
+                    transform.transform.rotation.w
+                )
+                print("yaw = {}".format(yaw))
 
+
+    def control_callback(self, control):
+        """
+
+        """
+        print("control = {}".format(control))
 
 
 def main():
