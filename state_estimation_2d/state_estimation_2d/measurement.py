8c9a72156daaa01f47bb1f2ea0da8aeaf6d13abf
Edd <paul.ev@fastsense.tech>
2021-06-09 14:42:44 +0000

    removed classes model2d and measurement2d

--- state_estimation_2d/state_estimation_2d/measurement.py
+++ state_estimation_2d/state_estimation_2d/measurement.py
@@ -1,32 +1,5 @@
 import numpy as np
 
-class Measurement2D:
-    def __init__(self, R):
-        self.z_odom = np.zeros(3)
-        self.z_model = np.zeros(3)
-        self.z_imu = 0
-        self.J_odom = np.zeros((3, 8))
-        self.J_model = np.zeros((3, 8))
-        self.J_imu = np.zeros((1, 8))
-    
-    def set_z_odom(self, z):
-        self.z_odom = z
-
-    def set_z_model(self, z):
-        self.z_model = z
-    
-    def set_z_imu(self, z):
-        self.z_imu = z
-    
-    def get_z_odom(self):
-        return self.z_odom
-    
-    def get_z_imu(self):
-        return self.z_imu
-
-    def get_z_model(self):
-        return self.z_model
-
 def get_jacobian_odom():
     J_odom = np.zeros((3, 8))
     J_odom[0, 2] = 1
--- state_estimation_2d/state_estimation_2d/model.py
+++ state_estimation_2d/state_estimation_2d/model.py
@@ -1,45 +1,4 @@
 import numpy as np
-
-
-class Model2D:
-    def __init__(self, Q, P_init, dt = 1):
-        self.x_predict = np.zeros(8)
-        self.Q = Q
-        self.P_predict = P_init
-        self.dt = dt
-        self.J_f = np.eye(8)
-        self.transform_jacobian()
-    
-    def set_state_vector(self, x):
-        self.x = x
-
-    def predict(self, x):
-        self.predict_state(x)
-        self.predict_covariance()
-        return self.x_predict, self.P_predict
-
-    def predict_state(self, x):
-        #Take previous optimal state
-        pos = x[0:2]
-        vel = x[2:4]
-        acc = x[4:6]
-        angle = x[6]
-        w = x[7]
-        #Calculate next state using model equations
-        next_pos = pos + vel * self.dt + 0.5 * acc * self.dt * self.dt
-        next_vel = vel + self.dt * acc
-        next_acc = acc
-        next_angle = angle + w * self.dt
-        next_w = w
-        #Fill the state vector
-        self.x_predict[0:2] = next_pos
-        self.x_predict[2:4] = next_vel
-        self.x_predict[4:6] = next_acc
-        self.x_predict[6] = next_angle
-        self.x_predict[7] = next_w
-    
-    def predict_covariance(self, P):
-        self.P_predict = self.J_f @ P @ self.J_f.T + self.Q
     
 def transform_jacobian(dt):
     J_f = np.eye(8)
