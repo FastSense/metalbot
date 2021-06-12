332bbce2c1acbba2278cc6a94b5b0a305385c1a3
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-06-12 11:31:42 +0700

    little fix

--- docker-gazebo/scripts/ros-setup.sh
+++ docker-gazebo/scripts/ros-setup.sh
@@ -3,4 +3,4 @@ echo "source /opt/ros/foxy/setup.zsh" >> ~/.zshrc && \
 echo "source ~/ros2_ws/install/setup.zsh" >> ~/.zshrc && \
 echo "export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11" >> ~/.zshrc && \
 echo "export GAZEBO_RESOURCE_PATH=/usr/share/gazebo-11/models" >> ~/.zshrc && \
-echo "export PYTHONPATH=/usr/local/lib/python3.8/dist-packages/cv2/python-3.8/" >> ~/.zshrc && \
+echo "export PYTHONPATH="/usr/local/lib/python3.8/dist-packages/cv2/python-3.8/:$PYTHONPATH"" >> ~/.zshrc
