2216cc876797d43b63d6e3578356895dd3ffa410
Edd <paul.ev@fastsense.tech>
2021-06-25 11:49:25 +0000

    added comments on odometry noiser node

--- odom_noiser/odom_noiser/noiser_node.py
+++ odom_noiser/odom_noiser/noiser_node.py
@@ -6,6 +6,29 @@ import numpy as np
 import random
 
 class Noiser(Node):
+    """
+    ROS node for adding noise to ground truth odometry
+    -----------------------------
+    Attributes:
+    odom_sub: ros::Subscriber
+        Subscriber to /odom topic for ground truth odometry
+    pose_pub: ros::Publisher
+        Publisher od noised odometry
+    odom_noised: Odometry
+        Noised odometry
+    x_sum: float
+        Integrated x coordinate
+    y_sum: float
+        Integrated y coordinate
+    theta_sum: float
+        Integrated yaw angle
+    dt: float
+        Time step corresponding to /odom publication frequency
+    lin_vel_noise_std: float
+        Standart deviation of linear velocity noise
+    rot_vel_noise_std: float
+        Standart deviation of angular velocity noise
+    """
     def __init__(self):
         super().__init__('odom_noiser')
 
@@ -14,9 +37,8 @@ class Noiser(Node):
             'odom',
             self.odometry_callback,
             10)
-        self.odom = Odometry()
-        self.odom_noised = Odometry()
         self.pose_pub = self.create_publisher(Odometry, '/odom_noised', 10)
+        self.odom_noised = Odometry()
         self.x_sum = 0
         self.y_sum = 0
         self.theta_sum = 0
@@ -29,10 +51,12 @@ class Noiser(Node):
         self.odom_noised.child_frame_id = msg.child_frame_id
         self.odom_noised.pose.covariance = msg.pose.covariance
         self.odom_noised.twist.covariance = msg.twist.covariance
+        # Noise adding process
         vel_noised_x = (msg.twist.twist.linear.x + 
                         random.gauss(0, self.lin_vel_noise_std))
         vel_noised_theta = (msg.twist.twist.angular.z + 
                             random.gauss(0, self.rot_vel_noise_std))
+        # Integration of velocities
         self.theta_sum = self.theta_sum + vel_noised_theta * self.dt
         self.x_sum = self.x_sum + vel_noised_x * np.cos(self.theta_sum) * self.dt
         self.y_sum = self.y_sum + vel_noised_x * np.sin(self.theta_sum) * self.dt
