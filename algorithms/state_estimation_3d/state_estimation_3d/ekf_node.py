fixed initial quaternion

algorithms/state_estimation_3d/state_estimation_3d/ekf_node.py
@@ -25,7 +25,7 @@ class EKFNode(Node):
         self.tf2_broadcaster = tf2_ros.TransformBroadcaster(self)
 
         # Declare parameters
-        self.declare_parameter('period', 0.1)
+        self.declare_parameter('period', 0.02)
         self.declare_parameter('vel_std', 1.0)
         self.declare_parameter('rot_vel_std', 1.0)
 
@@ -69,8 +69,6 @@ class EKFNode(Node):
 
         # Create Kalman filter
         self.tracker = SpaceKF12(dt=self.period, velocity_std=vel_std, rot_vel_std=rot_vel_std)
-        # theta = 45 * np.pi / 180
-        # self.tracker.q = np.array([np.cos(theta / 2), 0, 0, np.sin(theta / 2)])
         self.tracker.P = self.tracker.P * 0.01
 
         # Buffers for measurements
@@ -158,6 +156,13 @@ class EKFNode(Node):
         # Get extrinsics from tf
         extrinsic = self.get_extrinsic(msg.header.frame_id, 'base_link')
 
+        # Compute time delay
+        msg_time = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+        ros_stamp = self.get_clock().now().seconds_nanoseconds()
+        ros_time = ros_stamp[0] + ros_stamp[1] * 1e-9
+        delay = ros_time - msg_time
+        print('odom delay:', delay)
+
         self.tracker.update_flow(
             z,
             msg.delta_t,
@@ -167,6 +172,7 @@ class EKFNode(Node):
             self.stereo.M1,
             self.stereo.M1_inv,
             extrinsic=extrinsic,
+            delay=delay*0,
         )
 
     def get_extrinsic(self, frame1, frame2):

algorithms/state_estimation_3d/state_estimation_3d/spacekf/filter.py
@@ -60,7 +60,7 @@ class SpaceKF12:
             Q_discrete_white_noise(dim=2, dt=dt, var=velocity_std**2, block_size=3),
             Q_e_w, Q_e_w, Q_e_w,
         )
-        self.q = np.array([1., 0, 0, 0])
+        self.q = np.array([0., 0, 0, 1])
         self.dt = dt
         # Transition function
         self.transition_function = physics.transition_function12
@@ -103,7 +103,7 @@ class SpaceKF12:
         y = z - z_prior
         self.x, self.P = kalman_update(self.x, self.P, H, R, y)
 
-    def update_flow(self, flows, delta_t, depths, pixels, R, camera_matrix, camera_matrix_inv, extrinsic=None):
+    def update_flow(self, flows, delta_t, depths, pixels, R, camera_matrix, camera_matrix_inv, extrinsic=None, delay=0):
         '''
         Update state by a visual odometry measurement coming from a neural network.
 
@@ -116,11 +116,12 @@ class SpaceKF12:
             R (np.array of shape [3, 3]): error of measurements
             camera_matrix (np.array of shape [3, 3]): camera intrinsic matrix
             extrinsic (None or np.array of shape [3, 4]): optional. Camera pose relative to filter
+            delay (float): seconds passed between measurement being taken and it being processed
         '''
         z = flows.flatten()
         z_prior, H = measurent.flow_odom12(self.vel, self.rot_vel, self.q, delta_t, depths, pixels, camera_matrix, camera_matrix_inv, extrinsic)
         y = z - z_prior
-        noise = self.Q / self.dt * delta_t
+        noise = self.Q / self.dt * (delta_t + delay)
         self.x, self.P = kalman_update(self.x, self.P, H, R, y, noise)
 
     def reset_manifold(self):