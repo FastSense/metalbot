6e36a092132d1c0174c355e27b784c48d5c4135f
artofnothingness <speedy_sky@mail.ru>
2021-10-06 18:48:24 +0300

    add micro-ros

--- docker/drun.sh
+++ docker/drun.sh
@@ -35,8 +35,10 @@ run_docker()  {
     environment_args=()
 
     volumes_args+=( "-v /dev/bus/usb:/dev/bus/usb " )
-    volumes_args+=( "-v $HOME/.Xauthority:/home/user/.Xauthority:ro" )
 
+    local path="/home/user/micro_ros_ws/src/micro_ros_setup" 
+    volumes_args+=( "-v $(pwd)/../micro_ros_ws/src/micro_ros_setup:${path}:rw" )
+    volumes_args+=( "-v $HOME/.Xauthority:/home/user/.Xauthority:ro" )
     environment_args+=( "-v /dev/bus/usb:/dev/bus/usb " )
 
     if [[ "$USE_CUDA" == "ON" ]]; then
