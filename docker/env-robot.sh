009e0655e557e3189247db7e6f694a7c582ea92b
artofnothingness <speedy_sky@mail.ru>
2021-10-09 17:57:42 +0300

    update envs

--- docker/env-robot.sh
+++ docker/env-robot.sh
@@ -1,15 +1,15 @@
-export image=fastsense/robot_ros2
-export container=rosbot2
+export image=fastsense/metalbot:robot
+export container=metalbot
 
 export USE_X_SERVER=ON
 
-export USE_ROS1=OFF
+export USE_ROS1=ON
 export USE_ROS2=ON
 export USE_GAZEBO=OFF
 
+export USE_CUDA=ON
 export USE_TORCH=OFF
-export USE_CUDA=OFF
 export USE_OPENCV=OFF
 
-export USE_REALSENSE=OFF
+export USE_REALSENSE=ON
 export USE_OAKD=OFF
