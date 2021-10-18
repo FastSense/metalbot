430f41b46c15359feb0d69b792a586c535dbd745
MetalBot Alex <speedy_sky@mail.ru>
2021-10-18 13:08:08 +0200

    fixed bringup no tf

--- ros2_ws/src/rosbot/rosbot/launch/bringup_no_static_tf.launch.py
+++ ros2_ws/src/rosbot/rosbot/launch/bringup_no_static_tf.launch.py
@@ -8,31 +8,32 @@ from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 def generate_launch_description():
     use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
-    rosbot_description = get_package_share_directory('rosbot_description')
-    rplidar_ros = get_package_share_directory('rplidar_ros')
+    # rosbot_description = get_package_share_directory('rosbot_description')
+    # rplidar_ros = get_package_share_directory('rplidar_ros')
 
-    rosserial = launch_ros.actions.Node(
-        package='rosbot_description',
-        executable='rosserial_node.py',
-        output='screen',
-        parameters=[
-    		rosbot_description + '/config/rosserial.yaml'
-        ]
-    )
+    # rosserial = launch_ros.actions.Node(
+    #     package='rosbot_description',
+    #     executable='rosserial_node.py',
+    #     output='screen',
+    #     parameters=[
+    # 		rosbot_description + '/config/rosserial.yaml'
+    #     ]
+    # )
 
-    rosbot_tf = launch_ros.actions.Node(
-        package='rosbot_description',
-        executable='rosbot_tf',
-        output='log',
-    )
+    # rosbot_tf = launch_ros.actions.Node(
+    #     package='rosbot_description',
+    #     executable='rosbot_tf',
+    #     output='log',
+    # )
 
-    rp_lidar = launch.actions.IncludeLaunchDescription(
-        launch.launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(rplidar_ros, 'launch', 'rplidar.launch.py'))
-    )
+    # rp_lidar = launch.actions.IncludeLaunchDescription(
+    #     launch.launch_description_sources.PythonLaunchDescriptionSource(
+    #         os.path.join(rplidar_ros, 'launch', 'rplidar.launch.py'))
+    # )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -41,10 +42,10 @@ def generate_launch_description():
         ),
         DeclareLaunchArgument('verbose', default_value='true',
                               description='Set "true" to increase messages written to terminal.'),
-        rp_lidar,
-        laser_frame_tf,
-        rosserial,
-        rosbot_tf,
+        # rp_lidar,
+        # laser_frame_tf,
+        # rosserial,
+        # rosbot_tf,
                 Node(
             package='rosbot_controller',
             executable='publish_rosbot_tf',
