6592f00cf09d50fb12a2e608f780814d950b84a1
artofnothingness <speedy_sky@mail.ru>
2021-11-03 02:11:48 +0300

    update

--- ros2_ws/src/metalbot/metalbot_gazebo/launch/spawn.launch.py
+++ ros2_ws/src/metalbot/metalbot_gazebo/launch/spawn.launch.py
@@ -5,9 +5,7 @@ from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
-# If using xacro
-# import xacro
-
+import xacro
 import os
 
 def generate_launch_description():
@@ -23,29 +21,28 @@ def generate_launch_description():
     models_pkg = get_package_share_directory('metalbot_gazebo')
 
     # Xacro
-    # xacro_file = os.path.join(rosbot_description_dir, 'urdf', 'rosbot.xacro')
-    # xml = xacro.process_file(xacro_file).toxml()
+    xacro_file = os.path.join(models_pkg, 'urdf', 'rosbot.xacro')
+    xacro_xml = xacro.process_file(xacro_file).toxml()
 
     # Sdf or Urdf 
-    urdf = os.path.join(models_pkg, 'models', 'metalbot.sdf')
-    xml = open(urdf, 'r').read()
-
-    # Common part
-    xml = xml.replace('"', '\\"')
-    spawn_args = '{name: \"Robot\", xml: \"'  +  xml + '\" }'
-
-    description = {'robot_description': xml}
-    use_sim_time_param = {'use_sim_time': use_sim_time}
-    use_tf_static = {'use_tf_static': True}
-    publish_frequency = {'publish_frequency': 20.0}
+    sdf = os.path.join(models_pkg, 'models', 'metalbot.sdf')
+    sdf_xml = open(sdf, 'r').read()
+    sdf_xml = sdf_xml.replace('"', '\\"')
+    spawn_args = '{name: \"robot\", xml: \"'  +  sdf_xml + '\" }'
 
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[description, use_sim_time_param, use_tf_static, publish_frequency]
+        parameters=[
+            {'robot_description': xacro_xml},
+            {'use_sim_time': use_sim_time},
+            {'use_tf_static': True},
+            {'publish_frequency': 20.0}]
     )
 
+    spawn_process = ExecuteProcess(cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', spawn_args], 
+            output='screen')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -57,6 +54,6 @@ def generate_launch_description():
             default_value=default_update_rate,
             description=''
         ),
-        ExecuteProcess(cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', spawn_args], output='screen'),
         robot_state_publisher,
+        spawn_process
     ])
