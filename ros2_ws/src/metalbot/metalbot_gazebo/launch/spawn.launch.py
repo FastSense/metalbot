c8af57010543cceea01533c7f572dc49f4ae80a1
artofnothingness <speedy_sky@mail.ru>
2021-11-02 19:14:47 +0300

    working tf, xacro, sdf

--- ros2_ws/src/metalbot/metalbot_gazebo/launch/spawn.launch.py
+++ ros2_ws/src/metalbot/metalbot_gazebo/launch/spawn.launch.py
@@ -1,11 +1,17 @@
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
+# If using xacro
+# import xacro
+
+import os
+
 def generate_launch_description():
+    robot_name = 'metalbot'
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
     default_update_rate = '10'
@@ -14,88 +20,43 @@ def generate_launch_description():
         default=default_update_rate
     )
 
+    models_pkg = get_package_share_directory('metalbot_gazebo')
+
+    # Xacro
+    # xacro_file = os.path.join(rosbot_description_dir, 'urdf', 'rosbot.xacro')
+    # xml = xacro.process_file(xacro_file).toxml()
+
+    # Sdf or Urdf 
+    urdf = os.path.join(models_pkg, 'models', 'metalbot.sdf')
+    xml = open(urdf, 'r').read()
+
+    # Common part
+    xml = xml.replace('"', '\\"')
+    spawn_args = '{name: \"Robot\", xml: \"'  +  xml + '\" }'
+
+    description = {'robot_description': xml}
+    use_sim_time_param = {'use_sim_time': use_sim_time}
+    use_tf_static = {'use_tf_static': True}
+    publish_frequency = {'publish_frequency': 20.0}
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[description, use_sim_time_param, use_tf_static, publish_frequency]
+    )
+
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'),
         DeclareLaunchArgument(
             'odom_update_rate',
             default_value=default_update_rate,
             description=''
         ),
-
-        Node(
-            package='metalbot_gazebo',
-            executable='spawn_robot.py',
-            name='spawn_robot',
-            output='screen',
-            parameters=[{"odom_update_rate":odom_update_rate},],
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '-1.5707', '0', '-1.5707', 'camera_link', 'camera_depth_frame'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'dummy'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0.2', '0', '0.8', '0', '0', '0', 'base_link', 'camera_rgb_frame'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'range_fl'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'range_fr'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'range_rl'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'range_rr'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'top'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['-0.03', '0', '0.18', '0', '0', '0', 'base_link', 'camera_link'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            output='screen',
-            arguments=['0', '0', '0.07', '0', '0', '0', 'base_link', 'laser'],
-            parameters=[{'use_sim_time': use_sim_time}],
-            ),
+        ExecuteProcess(cmd=['ros2', 'service', 'call', '/spawn_entity', 'gazebo_msgs/SpawnEntity', spawn_args], output='screen'),
+        robot_state_publisher,
     ])
