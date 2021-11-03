59d279ec6d292ba77252c1b96f246529fd78d647
MetalBot Alex <speedy_sky@mail.ru>
2021-11-03 14:56:27 +0100

    add micro_ros to bringup

--- ros2_ws/src/metalbot/metalbot/launch/bringup.launch.py
+++ ros2_ws/src/metalbot/metalbot/launch/bringup.launch.py
@@ -1,4 +1,3 @@
-from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 from launch import LaunchDescription
 from launch_ros.substitutions import FindPackageShare
@@ -7,15 +6,20 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
+    serial = LaunchConfiguration('serial', default='/dev/ttyACM0')
+
     robot_pkg        = FindPackageShare('metalbot')
     robot_state_publisher_launch = PathJoinSubstitution([robot_pkg, 'launch', 'robot_state_publisher.launch.py'])
+    micro_ros_launch = PathJoinSubstitution([robot_pkg, 'launch', 'micro_ros.launch.py'])
 
     return LaunchDescription([
+        DeclareLaunchArgument('serial', default_value='/dev/ttyACM0', description='Serial port'),
         DeclareLaunchArgument(name='use_sim_time', default_value='false'),
         DeclareLaunchArgument('verbose', default_value='true', 
             description='Set "true" to increase messages written to terminal.'),
 
-        IncludeLaunchDescription(PythonLaunchDescriptionSource(robot_state_publisher_launch))
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(robot_state_publisher_launch)),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(micro_ros_launch))
     ])
 
 if __name__ == '__main__':
