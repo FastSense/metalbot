6aebc856aa7c5fb6848382f8571cd7f011a26978
KostyaYamshanov <k.yamshanov@fastsense.tech>
2021-11-11 02:19:26 +0700

    kostya_wip, add fill_path_with_way_points function, fix some bugs

--- ros2_ws/src/control/rosbot_controller/rosbot_controller/path_publisher.py
+++ ros2_ws/src/control/rosbot_controller/rosbot_controller/path_publisher.py
@@ -32,11 +32,13 @@ class TrajPublish(Node):
         super().__init__(node_name)
         self.declare_and_get_parameters()
 
-        self.trajectory = Trajectory()
-        self.trajectory.set_step(0.1)
-        self.trajectory.set_frame("odom")
-
+        self.trajectory = Trajectory(
+            step=0.1,
+            frame=self.path_frame
+        )
         self.path_pub = self.create_publisher(Path, self.path_topic, 5)
+        self.dt = 0.2
+        self.prepare_trajectory()
 
     def declare_and_get_parameters(self):
         """
@@ -46,237 +48,39 @@ class TrajPublish(Node):
         self.declare_parameter('move_plan')
         self.declare_parameter('num_of_subs', 1)
         self.declare_parameter('path_topic', '/path')
+        self.declare_parameter('path_frame', 'odom')
 
-        self.path_topic = self.get_parameter(
-            'path_topic').get_parameter_value().string_value
         self.traj_type = self.get_parameter(
             'traj_type').get_parameter_value().string_value
         self.move_plan = self.get_parameter(
             'move_plan').get_parameter_value().string_value
+        self.path_frame = self.get_parameter(
+            'path_frame').get_parameter_value().string_value
+        self.path_topic = self.get_parameter(
+            'path_topic').get_parameter_value().string_value
         self.num_of_subs = self.get_parameter(
             'num_of_subs').get_parameter_value().double_value
 
-    def generate_message(self):
+    def prepare_trajectory(self):
         """
-        Generating a message depending on its type
-
         """
-        if "sin" in self.traj_type:
-            amplitude, freq, reverse = self.parse_sin_traj()
-            self.SinTrajGenerator(amplitude, freq, reverse)
-        elif "spiral" in self.traj_type:
-            amplitude = self.parse_spiral_traj()
-            self.SpiralTrajGenerator(amplitude)
-        elif self.traj_type == "polygon":
-            self.PolygonTrajGenerator()
-        elif self.traj_type == "from_file":
-            self.FromFileTrajGenerator()
+        if self.traj_type == "from_file":
+            self.trajectory.parse_move_plan(self.move_plan)
+        else:
+            self.trajectory.parse_string(self.traj_type)
 
     def run(self):
         """
         Main function. Check the correctness of a type_traj, generate message
         and publish it in topic
-
         """
 
         while self.path_pub.get_subscription_count() < self.num_of_subs:
-            time.sleep(0.2)
+            time.sleep(self.dt)
 
         self.path_pub.publish(self.msg)
-
         self.destroy_node()
         rclpy.shutdown()
-
-    def is_valid_traj_type(self):
-        """
-        Checking the validity of a trajectory type
-
-        """
-        return 'sin' in self.traj_type or self.traj_type in ('from_file', 'polygon')\
-            or 'spiral' in self.traj_type
-
-    def parse_plan(self):
-        """
-        Parse path from file
-        Return:
-            edges: list of (x, y) - points of trajectory
-
-        """
-        edges = list()
-        if self.move_plan == '':
-            print("Error file_name")
-            return 1
-        with (pathlib.Path(self.move_plan)).open() as f:
-            for line in f:
-                p = line.replace('\n', '')
-                if p:
-                    p = p.split(" ")
-                    edges.append((float(p[0]), float(p[1])))
-
-        return edges
-
-    def edges_to_points(self, edges):
-        """
-        Creates array of points between the edges
-        Args:
-            edges: list of (x, y) - points of trajectory
-        Return:
-            points: big array of points - splitting edges into small edges
-
-        """
-        points = list()
-        p1 = (0.0, 0.0)
-
-        for edge in edges:
-            p2 = edge
-            k = (p2[1] - p1[1]) / (p2[0] - p1[0])
-            b = (p1[1]*p2[0] - p2[1]*p1[0]) / (p2[0] - p1[0])
-            x = p1[0]
-            y = k*x + b
-            points.append((x, y))
-            step = abs(p2[0] - p1[0])/10
-            if p2[0] > p1[0]:
-                while (x < p2[0]):
-                    x += step
-                    if (x > p2[0]):
-                        break
-                    y = k*x + b
-                    points.append((x, y))
-            else:
-                while (x > p2[0]):
-                    x -= step
-                    if (x < p2[0]):
-                        break
-                    y = k*x + b
-                    points.append((x, y))
-            p1 = p2
-
-        return points
-
-    def FromFileTrajGenerator(self):
-        """
-        Generating a trajectory from file
-
-        """
-        if self.move_plan is None:
-            print("Move plan was not specified")
-            return 1
-
-        edges = self.parse_plan()
-        points = self.edges_to_points(edges)
-
-        for p in points:
-            ps = PoseStamped()
-            ps.header = self.msg.header
-            ps.pose.position.x = p[0]
-            ps.pose.position.y = p[1]
-            ps.pose.position.z = 0.0
-            self.msg.poses.append(ps)
-
-    def SinTrajGenerator(self, a=1.0, f=1.0, reverse=False):
-        """
-        Generating a sinus trajectory (a*sin(f*t))
-        Args:
-            a: amplitude of a sinus
-            f: phase of a sinus
-
-        """
-        K = -1 if reverse == True else 1
-        x_ar = np.arange(0, 2*np.pi * K, self.step * K,
-                         dtype=float)   # start,stop,step
-        y_ar = float(a) * np.sin(float(f) * x_ar)
-        yaw_arr = float(a) * float(f) * np.cos(float(f) * x_ar)
-
-        for i in range(len(x_ar)):
-            ps = PoseStamped()
-            ps.header = self.msg.header
-            ps.pose.position.x = x_ar[i]
-            ps.pose.position.y = y_ar[i]
-            ps.pose.position.z = 0.0
-            goal_quaternion = list(Rotation.from_euler(
-                'z', math.atan(yaw_arr[i]), degrees=False
-            ).as_quat())
-            ps.pose.orientation.x = goal_quaternion[0]
-            ps.pose.orientation.y = goal_quaternion[1]
-            ps.pose.orientation.z = goal_quaternion[2]
-            ps.pose.orientation.w = goal_quaternion[3]
-            self.msg.poses.append(ps)
-
-    def PolygonTrajGenerator(self):
-        """
-        Generating a trajectory of the square
-
-        """
-        p_edges = [(1.0, -0.1), (1.1, 1.9),  (0.1, 1.0), (0, 0)]  # square
-        points = self.edges_to_points(p_edges)
-
-        for p in points:
-            ps = PoseStamped()
-            ps.header = self.msg.header
-
-            ps.pose.position.x = p[0]
-            ps.pose.position.y = p[1]
-            ps.pose.position.z = 0.0
-            goal_quaternion = list(Rotation.from_euler(
-                'z', math.atan2(p[1], p[0]), degrees=False
-            ).as_quat())
-            ps.pose.orientation.x = goal_quaternion[0]
-            ps.pose.orientation.y = goal_quaternion[1]
-            ps.pose.orientation.z = goal_quaternion[2]
-            ps.pose.orientation.w = goal_quaternion[3]
-            self.msg.poses.append(ps)
-
-    def SpiralTrajGenerator(self, amplitude):
-        """
-        Generating a spiral trajectory
-        Args:
-            amplitude: amplitude of spiral
-
-        """
-        key_points = []
-        if amplitude > 0:
-            k = 1
-        else:
-            k = -1
-        amplitude = abs(amplitude)
-        f = 0
-        while 1:
-            r = self.step * math.exp(f*0.1)
-            x = k * r * math.cos(f)
-            y = r * math.sin(f)
-            if abs(x) > amplitude or abs(y) > amplitude:
-                points = self.edges_to_points(key_points)
-                for p in points:
-                    ps = PoseStamped()
-                    ps.header = self.msg.header
-                    ps.pose.position.x = p[0]
-                    ps.pose.position.y = p[1]
-                    ps.pose.position.z = 0.0
-                    self.msg.poses.append(ps)
-                return
-            else:
-                key_points.append([x, y])
-                f += 0.1
-
-    def parse_sin_traj(self):
-        """
-        Parsing sinus trajectory
-
-        """
-        traj_type = self.traj_type.strip().split('sin')  # []
-        period = float(traj_type[-1])
-        amplitude = float(traj_type[0].split("_")[-1])
-        reverse = traj_type[0].split("_")[0] == 'reverse'
-        return amplitude, period, reverse
-
-    def parse_spiral_traj(self):
-        """
-        Parsing spiral trajectory
-
-        """
-        coef = self.traj_type.split('spiral')
-        amp = coef[0] if coef[0] != '' or coef[0] is None else 1.0
-        return float(amp)
 
 
 def main():
--- ros2_ws/src/control/rosbot_controller/rosbot_controller/robot_trajectory.py
+++ ros2_ws/src/control/rosbot_controller/rosbot_controller/robot_trajectory.py
@@ -4,7 +4,10 @@ import pathlib
 import numpy as np
 import math
 import time
+import errno
+import os
 from scipy.spatial.transform import Rotation
+from rosbot_controller.rosbot_2D import Goal
 
 # https://axiacore.com/blog/mathematical-expression-evaluator-python-524/
 # https://nerdparadise.com/programming/parsemathexpr
@@ -19,11 +22,16 @@ class Trajectory():
 
     """
 
-    def __init__(self, path=Path()):
+    def __init__(self, path=Path(), step=0.1, frame="odom"):
         """
         """
         self.points_ = path
-        self.step_ = 0.1
+        self.step_ = step
+        self.points_.header.frame_id = frame
+        self.default_polygonal_way_points = [
+            (2.0, 0.0), (2.0, 2.0),  (0.0, 2.0), (0, 0)
+        ]
+        self.default_sin = "1.0sin1.0"
 
     def set_step(self, new_step: float):
         """
@@ -40,219 +48,200 @@ class Trajectory():
         """
         return self.points_
 
-    def set_path(self):
+    def set_path(self, path: Path):
+        """
+        """
+        self.points_ = path
+
+    def parse_string(self, input_str):
         """
         """
         pass
 
+    def parse_move_plan(self, mp_path):
+        """
+        """
+        way_points = list()
+        with (pathlib.Path(mp_path)).open() as f:
+            for line in f:
+                way_points.append(
+                    self.str_to_way_point(line)
+                )
 
-class SinTrajectory():
-    pass
+        self.fill_path_with_way_points(way_points)
 
+    def str_to_way_point(self, line):
+        """
+        """
+        line = [float(l) for l in line.split(" ")]
+        return Goal(line[0], line[1])
 
-class PolygonTrajectory():
-    pass
+    def fill_path_with_way_points(self, way_points):
+        """
+        """
+        prev_wp = way_points[0]
 
+        for next_wp in way_points[1:]:
 
-class PointsTrajectory():
-    pass
+            line_along_y_axis = self.is_line_along_y_axis(prev_wp, next_wp)
+            if not line_along_y_axis:
+                k, b = self.calculate_line_coefficients(prev_wp, next_wp)
 
+            x = prev_wp.x
+            y = prev_wp.y if line_along_y_axis else k * x + b
+            yaw_quat = self.calculate_yaw_quat_for_points((prev_wp, next_wp))
+            self.add_point_to_path(x, y, yaw_quat)
 
-def is_valid_traj_type(self):
-    """
-    Checking the validity of a trajectory type
+            step = min(abs(next_wp.x - prev_wp.x), self.step)
+            if not line_along_y_axis:
+                if next_wp.x > prev_wp.x:
+                    while (x < next_wp.x-step):
+                        x += step
+                        y = k * x + b
+                        self.add_point_to_path(x, y, yaw_quat)
+                else:
+                    while (x > next_wp.x+step):
+                        x -= step
+                        y = k * x + b
+                        self.add_point_to_path(x, y, yaw_quat)
+            else:
+                if next_wp.y > prev_wp.y:
+                    while (y < next_wp.y-step):
+                        y += step
+                        self.add_point_to_path(x, y, yaw_quat)
+                else:
+                    while (y > next_wp.y+step):
+                        y -= step
+                        self.add_point_to_path(x, y, yaw_quat)
 
-    """
-    return 'sin' in self.traj_type or self.traj_type in ('from_file', 'polygon')\
-        or 'spiral' in self.traj_type
+            prev_wp = next_wp
 
+    def add_point_to_path(self, x, y, yaw_quat):
+        """
+        """
+        point = PoseStamped()
+        point.header = self.msg.header
+        point.pose.position.x = x
+        point.pose.position.y = y
+        point.pose.position.z = 0.0
+        point.pose.orientation.x = yaw_quat[0]
+        point.pose.orientation.y = yaw_quat[1]
+        point.pose.orientation.z = yaw_quat[2]
+        point.pose.orientation.w = yaw_quat[3]
+        self.points_.poses.append(point)
 
-def parse_plan(self):
-    """
-    Parse path from file
-    Return:
-        edges: list of (x, y) - points of trajectory
+    def calculate_line_coefficients(prev_point, next_point):
+        """
+        """
+        k = (next_point.y - prev_point.y) / (next_point.x - prev_point.x)
 
-    """
-    edges = list()
-    if self.move_plan == '':
-        print("Error file_name")
-        return 1
-    with (pathlib.Path(self.move_plan)).open() as f:
-        for line in f:
-            p = line.replace('\n', '')
-            if p:
-                p = p.split(" ")
-                edges.append((float(p[0]), float(p[1])))
+        b = (prev_point.y*next_point.x - next_point.y *
+             prev_point.x) / (next_point.x - prev_point.x)
+        return k, b
 
-    return edges
+    def calculate_yaw_quat_for_points(self, prev_point, next_point):
+        """
+        """
+        yaw = math.atan2(next_point.y - prev_point.y,
+                         next_point.x - prev_point.x)
+        yaw_quat = Rotation.from_euler('z', yaw, degrees=False)
+        yaw_quat = list(yaw_quat.as_quat())
+        return yaw_quat
 
+    def is_line_along_y_axis(self, prev_point, next_point):
+        """
+        """
+        return next_point.x - prev_point.x == 0
 
-def edges_to_points(self, edges):
-    """
-    Creates array of points between the edges
-    Args:
-        edges: list of (x, y) - points of trajectory
-    Return:
-        points: big array of points - splitting edges into small edges
+    def Set_polygonal_path(self, way_points_list=None):
+        """
+        """
+        way_points = list()
+        if way_points_list is None:
+            way_points_list = self.default_polygonal_way_points
 
-    """
-    points = list()
-    p1 = (0.0, 0.0)
+        for point in way_points_list:
+            way_points.append(Goal(point[0], point[1]))
 
-    for edge in edges:
-        p2 = edge
-        k = (p2[1] - p1[1]) / (p2[0] - p1[0])
-        b = (p1[1]*p2[0] - p2[1]*p1[0]) / (p2[0] - p1[0])
-        x = p1[0]
-        y = k*x + b
-        points.append((x, y))
-        step = abs(p2[0] - p1[0])/10
-        if p2[0] > p1[0]:
-            while (x < p2[0]):
-                x += step
-                if (x > p2[0]):
-                    break
-                y = k*x + b
-                points.append((x, y))
-        else:
-            while (x > p2[0]):
-                x -= step
-                if (x < p2[0]):
-                    break
-                y = k*x + b
-                points.append((x, y))
-        p1 = p2
+        self.fill_path_with_way_points(way_points)
 
-    return points
+        # def SinTrajGenerator(self, a=1.0, f=1.0, reverse=False):
+        #     """
+        #     Generating a sinus trajectory (a*sin(f*t))
+        #     Args:
+        #         a: amplitude of a sinus
+        #         f: phase of a sinus
 
+        #     """
+        #     K = -1 if reverse == True else 1
+        #     x_ar = np.arange(0, 2*np.pi * K, self.step * K,
+        #                      dtype=float)   # start,stop,step
+        #     y_ar = float(a) * np.sin(float(f) * x_ar)
+        #     yaw_arr = float(a) * float(f) * np.cos(float(f) * x_ar)
 
-def FromFileTrajGenerator(self):
-    """
-    Generating a trajectory from file
+        #     for i in range(len(x_ar)):
+        #         ps = PoseStamped()
+        #         ps.header = self.msg.header
+        #         ps.pose.position.x = x_ar[i]
+        #         ps.pose.position.y = y_ar[i]
+        #         ps.pose.position.z = 0.0
+        #         goal_quaternion = list(Rotation.from_euler(
+        #             'z', math.atan(yaw_arr[i]), degrees=False
+        #         ).as_quat())
+        #         ps.pose.orientation.x = goal_quaternion[0]
+        #         ps.pose.orientation.y = goal_quaternion[1]
+        #         ps.pose.orientation.z = goal_quaternion[2]
+        #         ps.pose.orientation.w = goal_quaternion[3]
+        #         self.msg.poses.append(ps)
 
-    """
-    if self.move_plan is None:
-        print("Move plan was not specified")
-        return 1
+        # def SpiralTrajGenerator(self, amplitude):
+        #     """
+        #     Generating a spiral trajectory
+        #     Args:
+        #         amplitude: amplitude of spiral
 
-    edges = self.parse_plan()
-    points = self.edges_to_points(edges)
+        #     """
+        #     key_points = []
+        #     if amplitude > 0:
+        #         k = 1
+        #     else:
+        #         k = -1
+        #     amplitude = abs(amplitude)
+        #     f = 0
+        #     while 1:
+        #         r = self.step * math.exp(f*0.1)
+        #         x = k * r * math.cos(f)
+        #         y = r * math.sin(f)
+        #         if abs(x) > amplitude or abs(y) > amplitude:
+        #             points = self.edges_to_points(key_points)
+        #             for p in points:
+        #                 ps = PoseStamped()
+        #                 ps.header = self.msg.header
+        #                 ps.pose.position.x = p[0]
+        #                 ps.pose.position.y = p[1]
+        #                 ps.pose.position.z = 0.0
+        #                 self.msg.poses.append(ps)
+        #             return
+        #         else:
+        #             key_points.append([x, y])
+        #             f += 0.1
 
-    for p in points:
-        ps = PoseStamped()
-        ps.header = self.msg.header
-        ps.pose.position.x = p[0]
-        ps.pose.position.y = p[1]
-        ps.pose.position.z = 0.0
-        self.msg.poses.append(ps)
+        # def parse_sin_traj(self):
+        #     """
+        #     Parsing sinus trajectory
 
+        #     """
+        #     traj_type = self.traj_type.strip().split('sin')  # []
+        #     period = float(traj_type[-1])
+        #     amplitude = float(traj_type[0].split("_")[-1])
+        #     reverse = traj_type[0].split("_")[0] == 'reverse'
+        #     return amplitude, period, reverse
 
-def SinTrajGenerator(self, a=1.0, f=1.0, reverse=False):
-    """
-    Generating a sinus trajectory (a*sin(f*t))
-    Args:
-        a: amplitude of a sinus
-        f: phase of a sinus
+        # def parse_spiral_traj(self):
+        #     """
+        #     Parsing spiral trajectory
 
-    """
-    K = -1 if reverse == True else 1
-    x_ar = np.arange(0, 2*np.pi * K, self.step * K,
-                     dtype=float)   # start,stop,step
-    y_ar = float(a) * np.sin(float(f) * x_ar)
-    yaw_arr = float(a) * float(f) * np.cos(float(f) * x_ar)
-
-    for i in range(len(x_ar)):
-        ps = PoseStamped()
-        ps.header = self.msg.header
-        ps.pose.position.x = x_ar[i]
-        ps.pose.position.y = y_ar[i]
-        ps.pose.position.z = 0.0
-        goal_quaternion = list(Rotation.from_euler(
-            'z', math.atan(yaw_arr[i]), degrees=False
-        ).as_quat())
-        ps.pose.orientation.x = goal_quaternion[0]
-        ps.pose.orientation.y = goal_quaternion[1]
-        ps.pose.orientation.z = goal_quaternion[2]
-        ps.pose.orientation.w = goal_quaternion[3]
-        self.msg.poses.append(ps)
-
-
-def PolygonTrajGenerator(self):
-    """
-    Generating a trajectory of the square
-
-    """
-    p_edges = [(1.0, -0.1), (1.1, 1.9),  (0.1, 1.0), (0, 0)]  # square
-    points = self.edges_to_points(p_edges)
-
-    for p in points:
-        ps = PoseStamped()
-        ps.header = self.msg.header
-
-        ps.pose.position.x = p[0]
-        ps.pose.position.y = p[1]
-        ps.pose.position.z = 0.0
-        goal_quaternion = list(Rotation.from_euler(
-            'z', math.atan2(p[1], p[0]), degrees=False
-        ).as_quat())
-        ps.pose.orientation.x = goal_quaternion[0]
-        ps.pose.orientation.y = goal_quaternion[1]
-        ps.pose.orientation.z = goal_quaternion[2]
-        ps.pose.orientation.w = goal_quaternion[3]
-        self.msg.poses.append(ps)
-
-
-def SpiralTrajGenerator(self, amplitude):
-    """
-    Generating a spiral trajectory
-    Args:
-        amplitude: amplitude of spiral
-
-    """
-    key_points = []
-    if amplitude > 0:
-        k = 1
-    else:
-        k = -1
-    amplitude = abs(amplitude)
-    f = 0
-    while 1:
-        r = self.step * math.exp(f*0.1)
-        x = k * r * math.cos(f)
-        y = r * math.sin(f)
-        if abs(x) > amplitude or abs(y) > amplitude:
-            points = self.edges_to_points(key_points)
-            for p in points:
-                ps = PoseStamped()
-                ps.header = self.msg.header
-                ps.pose.position.x = p[0]
-                ps.pose.position.y = p[1]
-                ps.pose.position.z = 0.0
-                self.msg.poses.append(ps)
-            return
-        else:
-            key_points.append([x, y])
-            f += 0.1
-
-
-def parse_sin_traj(self):
-    """
-    Parsing sinus trajectory
-
-    """
-    traj_type = self.traj_type.strip().split('sin')  # []
-    period = float(traj_type[-1])
-    amplitude = float(traj_type[0].split("_")[-1])
-    reverse = traj_type[0].split("_")[0] == 'reverse'
-    return amplitude, period, reverse
-
-
-def parse_spiral_traj(self):
-    """
-    Parsing spiral trajectory
-
-    """
-    coef = self.traj_type.split('spiral')
-    amp = coef[0] if coef[0] != '' or coef[0] is None else 1.0
-    return float(amp)
+        #     """
+        #     coef = self.traj_type.split('spiral')
+        #     amp = coef[0] if coef[0] != '' or coef[0] is None else 1.0
+        #     return float(amp)
